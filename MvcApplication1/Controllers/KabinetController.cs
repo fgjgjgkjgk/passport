c929d7e5c6bd00c29adde2fc19820a363e3f2e53
returnt <kryarek@mail.ru>
2014-11-24 00:01:28 +0200

    summ

---
+++ MvcApplication1/Controllers/KabinetController.cs
@@ -0,0 +1,53 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class KabinetController : Controller
+    {
+        //
+        // GET: /Kabinet/
+        private Entities3 db = new Entities3();
+      //  [Authorize(Roles = "user")]
+        [Authorize(Roles = "admin")] 
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        //[Authorize(Roles = "user")]
+        [Authorize(Roles = "admin")]
+        public ActionResult Create()
+        {
+            Kabinets kabinet = new Kabinets();
+
+            return View(kabinet);
+        }
+        [HttpPost]
+        public ActionResult Create(Kabinets kabinet)
+        {
+          
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Kabinets.Add(kabinet);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                ModelState.AddModelError(String.Empty, ex);
+                
+            }
+            return View(kabinet);
+        }
+    }
+}
