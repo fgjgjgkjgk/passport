d3ea790584fdb80ad0f1b64a2cc4b40aff462a65
returnt <kryarek@mail.ru>
2014-11-23 23:26:27 +0200

    starter

---
+++ MvcApplication1/Models/PersonRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    public class PersonRepository : Controller
+    {
+        //
+        // GET: /PersonRepository/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
