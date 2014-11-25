1d74885c0205e094d4eef6611b992519c4bb2cc0
returnt <returnt>
2014-11-25 15:54:09 +0200

    new proj

---
+++ MvcApplication1/Controllers/storeadmin/RevisionController.cs
@@ -0,0 +1,137 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers.storeadmin
+{
+    public class RevisionController : Controller
+    {
+
+        private Entities7 db = new Entities7();
+        //
+        // GET: /Revision/
+
+        public ActionResult Index()
+        {
+            var queri = (from u in db.gda_passport select u).ToList();
+            return View(queri);
+        }
+
+        //
+        // GET: /Revision/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var details = (from u in db.gda_passport
+                           where u.Id == id
+                           select u).First();
+            return View(details);
+        }
+
+        //
+        // GET: /Revision/Create
+
+        public ActionResult Create()
+        {
+            //gda_passport pasport = new gda_passport();
+            Response.Redirect("/Passport/extradition");
+            return View();
+        }
+
+        //
+        // POST: /Revision/Create
+
+        [HttpPost]
+        public ActionResult Create(gda_passport pasport)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    return RedirectToAction("Index");
+                }
+
+
+            }
+            catch
+            {
+
+            }
+            return View();
+        }
+
+        //
+        // GET: /Revision/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            var edit = (from u in db.gda_passport
+                        where u.Id == id
+                        select u).First();
+            return View(edit);
+        }
+
+        //
+        // POST: /Revision/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var edit = (from u in db.gda_passport
+                        where u.Id == id
+                        select u).First();
+
+            try
+            {
+                UpdateModel(edit);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(edit);
+            }
+        }
+
+        //
+        // GET: /Revision/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            var del = (from u in db.gda_passport
+                       where u.Id == id
+                       select u).First();
+            return View(del);
+        }
+
+        //
+        // POST: /Revision/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+
+            var del = (from u in db.gda_passport
+                       where u.Id == id
+                       select u).First();
+            try
+            {
+                db.gda_passport.Remove(del);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/Models/MyModel/GetUserInf.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using WebMatrix.WebData;
+
+namespace MvcApplication1.Models.MyModel
+{
+    public class GetUserInf
+    {
+
+        private Entities5 db_Kabinet = new Entities5();
+        private string conectString = @"Data Source=(LocalDb)\v11.0;Initial Catalog=aspnet-MvcApplication1-20141021183026;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\aspnet-MvcApplication1-20141021183026.mdf";
+        private List<String> queri = new List<String>();
+
+        private int UserId = WebSecurity.CurrentUserId;
+
+        public GetUserInf() { 
+              
+        }
+
+        public Array Getuser() {
+
+            var all = (from u in db_Kabinet.UserKabinet where u.UserId == UserId select u).ToArray();
+
+            return all;
+           
+        }
+
+        public List<String> SQLSelect(String colum, String table) {
+
+            try
+            {
+                SqlConnection conn = new SqlConnection(conectString);
+
+                try
+                {
+                    //пробуем подключится
+                    conn.Open();
+                }
+                catch (SqlException se)
+                {
+
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT " + colum + " FROM " + table, conn);
+                SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dr.Read())
+                {
+                    for (int i = 0; i <= dr.FieldCount; i++)
+                    {
+                        /*метод GetValue() класса SqlDataReader позволяет получить значение столбца
+                                                по номеру, который передается в качестве параметра, данному методу
+                                                и озночает номер столбца в таблице(начинается с 0)
+                                                */
+
+                        queri.Add(dr.GetValue(i++).ToString());
+
+                    }
+                }
+                conn.Close();
+                conn.Dispose();
+
+                return queri;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+         
+        }
+
+        public void SQLInsert(String TableName, String ColumnName, String Values) {
+
+            try
+            {
+                SqlConnection conn = new SqlConnection(conectString);
+
+                try
+                {
+                    //пробуем подключится
+                    conn.Open();
+                }
+                catch (SqlException se)
+                {
+
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO " + TableName + " (" + ColumnName + ") VALUES ( " + Values + " );", conn);
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+                conn.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
